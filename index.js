73951d51d8fc4ef70cf765ce08426f3241c7c343
fzd <fzddnp@hotmail.com>
2025-07-26 14:03:55 +0330

    Add grid

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
-const container = document.getElementById("grid");
+const container = document.querySelector(".container");
 
 for (let i = 0; i < 16 * 16; i++) {
   const square = document.createElement("div");
   square.classList.add("square");
   container.appendChild(square);
 }
+
+container.style.height = `${16 * 32}px`;
+container.style.width = `${16 * 32}px`;
