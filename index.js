f114bbf496d510a89e7c2726ee9da70e297fa6dd
fzd <fzddnp@hotmail.com>
2025-07-27 16:18:53 +0330

    Add button to change grid size

--- index.js
+++ index.js
@@ -1,16 +1,40 @@
 const container = document.querySelector(".container");
 
-for (let i = 0; i < 16 * 16; i++) {
-  const square = document.createElement("div");
-  square.classList.add("square");
-  container.appendChild(square);
-}
+function gridCreator(gridSize = 16) {
+  for (let i = 0; i < gridSize * gridSize; i++) {
+    const square = document.createElement("div");
+    square.classList.add("square");
+    container.appendChild(square);
+  }
 
-container.style.height = `${16 * 32}px`;
-container.style.width = `${16 * 32}px`;
+  const squares = document.querySelectorAll(".square");
+  squares.forEach((sqr) => {
+    sqr.style.height = `${640 / gridSize}px`;
+    sqr.style.width = `${640 / gridSize}px`;
+  });
+}
 
 container.addEventListener("mouseover", (e) => {
   let pixel = e.target;
   pixel.classList.add("fill");
   // pixel.style.backgroundColor = "black";
 });
+
+function gridSizeSelector() {
+  const size = +prompt("Enter the grid size:");
+  if (size > 100 || size < 2) {
+    alert("Grid size must range from 2 to 100.");
+  } else {
+    return size;
+  }
+}
+
+// const btn = document.getElementById("grid-size");
+const btn = document.querySelector("#grid-size");
+
+btn.addEventListener("click", () => {
+  container.innerHTML = "";
+  gridCreator(gridSizeSelector());
+});
+
+gridCreator();
